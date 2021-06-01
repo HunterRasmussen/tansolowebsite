b994ae6d88ea22778179c03602947a1298c81060
Hunter Rasmussen <hunterhrasmussen@gmail.com>
2021-06-01 17:04:20 -0600

    Adds image and changes select option.  Added javascript file to allow for dynamic image updating for guitar.  Only started on that

---
+++ imagebuilder.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    var guitarColor = document.getElementById("guitarColor");
+    var imageGuitarColor = document.getElementById("imageGuitarColor");
+    guitarColor.onchange = function(newcolor) {
+        console.log("working");
+    };
+});
+
