c8ca004882f33f3d1da8466896fd7c0d1d60720f
Hunter Rasmussen <hunterhrasmussen@gmail.com>
2021-06-10 17:25:05 -0600

    adds all necessary body photos fixes erros in index adds columns to builder

--- imagebuilder.js
+++ imagebuilder.js
@@ -3,7 +3,31 @@ $(document).ready(function(){
     var guitarColor = document.getElementById("guitarColor");
     var imageGuitarColor = document.getElementById("imageGuitarColor");
     guitarColor.onchange = function(newcolor) {
+        switch(guitarColor.value)
+        {
+            case 'olympicWhite':
+                imageGuitarColor.src = './images/guitars/body_olympicWhite.png'; 
+                break;
+            case 'desertSand':
+                imageGuitarColor.src = './images/guitars/body_desertSand.png'; 
+                break;
+            case 'agedWhite':
+                imageGuitarColor.src = './images/guitars/body_agedWhite.png'; 
+                break;
+            case 'seaFoamGreen':
+                imageGuitarColor.src = './images/guitars/body_seaFoamGreen.png'; 
+                break;
+            case 'sonicBlue':
+                imageGuitarColor.src = './images/guitars/body_sonicBlue.png'; 
+                break;
+            case 'shellPink':
+                imageGuitarColor.src = './images/guitars/body_shellPink.png'; 
+                break;
+           default:
+                break;
+        }
         console.log("working");
     };
 });
 
+//<img id=\"imageGuitarColor\" src=\"./images/guitars/desertsand.png\">"
