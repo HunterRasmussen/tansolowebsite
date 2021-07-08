2d66cb5a025ee5d680aa8d4f68aca934dde28b77
Hunter Rasmussen <hunterhrasmussen@gmail.com>
2021-07-07 20:29:13 -0600

    Adds features to builder form including all the images needed as well as an email service through emailjs

--- imagebuilder.js
+++ imagebuilder.js
@@ -1,7 +1,25 @@
 
+
+(function() {
+    // https://dashboard.emailjs.com/admin/integration
+    emailjs.init('user_PmoG01lIJoHialW2zyfYY');
+})();
+    
+
 $(document).ready(function(){
     var guitarColor = document.getElementById("guitarColor");
     var imageGuitarColor = document.getElementById("imageGuitarColor");
+
+    var imagePickguardColor = document.getElementById("imagePickguardColor");
+    var pickguardColor = document.getElementById("pickguard");
+
+    var  imageGuitarNeck = document.getElementById("imageGuitarNeck");
+    var guitarNeck = document.getElementById("neck");
+
+    var imageGuitarPickup = document.getElementById("imageGuitarPickup");
+    var guitarPickup = document.getElementById("pickup");
+
+    
     guitarColor.onchange = function(newcolor) {
         switch(guitarColor.value)
         {
@@ -28,6 +46,71 @@ $(document).ready(function(){
         }
         console.log("working");
     };
+
+
+    pickguardColor.onchange = function(newcolor) {
+        switch(pickguardColor.value)
+        {
+            case 'black':
+                imagePickguardColor.src = './images/guitars/pickguard_black.png'; 
+                break;
+            case 'white':
+                imagePickguardColor.src = './images/guitars/pickguard_white.png'; 
+                break;
+            case 'parchment':
+                imagePickguardColor.src = './images/guitars/pickguard_parchment.png'; 
+                break;
+            case 'mint':
+                imagePickguardColor.src = './images/guitars/pickguard_mint.png'; 
+                break;
+           default:
+                break;
+        }
+        console.log("working");
+    };
+
+
+    guitarNeck.onchange = function() {
+        switch(guitarNeck.value)
+        {
+            case 'maple':
+                imageGuitarNeck.src = './images/guitars/neck_maple.png';
+                break;
+            case 'rosewood':
+                imageGuitarNeck.src = './images/guitars/neck_rosewood.png';
+                break;
+            default:
+                break;
+        }
+    }
+
+    guitarPickup.onchange = function() {
+        switch(guitarPickup.value)
+        {
+            case 'seyDuncBroadcaster':
+                imageGuitarPickup.src = './images/guitars/pickup_seymour_duncan_broadcaster.png';
+                break;
+            case 'seyDuncJerryDonahue':
+                imageGuitarPickup.src = './images/guitars/pickup_seymour_duncan_jerry_donahue.png';
+                break;
+        }
+    }
+
 });
+
+window.onload = function() {
+            document.getElementById('builderForm').addEventListener('submit', function(event) {
+                event.preventDefault();
+                // generate a five digit number for the contact_number variable
+                this.contact_number.value = Math.random() * 100000 | 0;
+                // these IDs from the previous steps
+                emailjs.sendForm('builderForm_service', 'builder_form', this)
+                    .then(function() {
+                        console.log('SUCCESS!');
+                    }, function(error) {
+                        console.log('FAILED...', error);
+                    });
+            });
+        }
 
 //<img id=\"imageGuitarColor\" src=\"./images/guitars/desertsand.png\">"
